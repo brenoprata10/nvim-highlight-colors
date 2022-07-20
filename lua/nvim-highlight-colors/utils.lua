4500b6d58b97309effe40fe6698215cfe4095f2b
Breno Prata <brenoprata@hotmail.com>
2022-07-20 15:33:24 +0200

    Add support to shorthand hex

--- lua/nvim-highlight-colors/utils.lua
+++ lua/nvim-highlight-colors/utils.lua
@@ -1,5 +1,23 @@
 local M = {}
 
+function M.is_short_hex_color(color)
+	return string.len(color) == 4
+end
+
+function M.convert_short_hex_to_hex(color)
+	if (M.is_short_hex_color(color)) then
+		local new_color = "#"
+		for char in color:gmatch"." do
+			if (char ~= '#') then
+				new_color = new_color .. char:rep(2)
+			end
+		end
+		return new_color
+	end
+
+	return color
+end
+
 function M.get_buffer_contents(min_row, max_row)
 	return vim.api.nvim_buf_get_lines(0, min_row, max_row, false)
 end
@@ -48,7 +66,13 @@ function M.create_window(row, col, bg_color)
 		noautocmd = true,
 		zindex = 1,
 	})
-	vim.api.nvim_command("highlight " .. highlight_color_name .. " guibg=" .. bg_color)
+	vim.api.nvim_command(
+		"highlight " .. highlight_color_name .. " guibg=" .. (
+			M.is_short_hex_color(bg_color)
+				and M.convert_short_hex_to_hex(bg_color)
+				or bg_color
+		)
+	)
 	vim.api.nvim_win_set_option(window, 'winhighlight', 'Normal:' .. highlight_color_name .. ',FloatBorder:' .. highlight_color_name)
 	return window
 end
