ee717d60e668da44b726d2262a28bb99995a8462
CPea <vienpro2506@gmail.com>
2022-08-08 00:35:43 +0700

    #5 - implement new algorithm for better contrast

--- lua/nvim-highlight-colors/colors.lua
+++ lua/nvim-highlight-colors/colors.lua
@@ -1,5 +1,5 @@
 local buffer_utils = require("nvim-highlight-colors.buffer_utils")
-local table_utils  = require("nvim-highlight-colors.table_utils")
+local table_utils = require("nvim-highlight-colors.table_utils")
 
 local M = {}
 
@@ -10,30 +10,25 @@ M.var_declaration_regex = M.var_regex .. ":%s*" .. M.hex_regex
 M.var_usage_regex = "var%(" .. M.var_regex .. "%)"
 
 function M.get_color_value(color, row_offset)
-	if (M.is_short_hex_color(color)) then
+	if M.is_short_hex_color(color) then
 		return M.convert_short_hex_to_hex(color)
 	end
 
-	if (M.is_rgb_color(color)) then
+	if M.is_rgb_color(color) then
 		local rgb_table = M.get_rgb_values(color)
-		if (#rgb_table >= 3) then
+		if #rgb_table >= 3 then
 			return M.convert_rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
 		end
 	end
 
-	if (M.is_var_color(color)) then
+	if M.is_var_color(color) then
 		local var_name = string.match(color, M.var_regex)
 		local var_name_regex = string.gsub(var_name, "%-", "%%-")
-		local var_position = buffer_utils.get_positions_by_regex(
-			{
-				var_name_regex .. ":%s*" .. M.hex_regex,
-				var_name_regex .. ":%s*" .. M.rgb_regex
-			},
-			0,
-			vim.fn.line('$'),
-			row_offset
-		)
-		if (#var_position > 0) then
+		local var_position = buffer_utils.get_positions_by_regex({
+			var_name_regex .. ":%s*" .. M.hex_regex,
+			var_name_regex .. ":%s*" .. M.rgb_regex,
+		}, 0, vim.fn.line("$"), row_offset)
+		if #var_position > 0 then
 			local hex_color = string.match(var_position[1].value, M.hex_regex)
 			local rgb_color = string.match(var_position[1].value, M.rgb_regex)
 			return hex_color and M.get_color_value(hex_color) or M.get_color_value(rgb_color)
@@ -44,7 +39,17 @@ function M.get_color_value(color, row_offset)
 end
 
 function M.convert_rgb_to_hex(r, g, b)
- 	return string.format("#%02X%02X%02X", r, g, b)
+	return string.format("#%02X%02X%02X", r, g, b)
+end
+
+function M.convert_hex_to_rgb(hex)
+	if M.is_short_hex_color(hex) then
+		hex = M.convert_short_hex_to_hex(hex)
+	end
+
+	hex = hex:gsub("#", "")
+
+	return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
 end
 
 function M.is_short_hex_color(color)
@@ -60,10 +65,10 @@ function M.is_var_color(color)
 end
 
 function M.convert_short_hex_to_hex(color)
-	if (M.is_short_hex_color(color)) then
+	if M.is_short_hex_color(color) then
 		local new_color = "#"
-		for char in color:gmatch"." do
-			if (char ~= '#') then
+		for char in color:gmatch(".") do
+			if char ~= "#" then
 				new_color = new_color .. char:rep(2)
 			end
 		end
@@ -83,28 +88,29 @@ function M.get_rgb_values(color)
 end
 
 function M.get_foreground_color_from_hex_color(color)
-	local color_score = 0
-	local color_threshhold = 40
+	local rgb_table = { M.convert_hex_to_rgb(color) }
 
-	for value in string.gmatch(color, ".") do
-		if type(value) == "number" then
-			color_score = color_score + value
-		elseif string.lower(value) == 'a' then
-			color_score = color_score + 10
-		elseif string.lower(value) == 'b' then
-			color_score = color_score + 11
-		elseif string.lower(value) == 'c' then
-			color_score = color_score + 12
-		elseif string.lower(value) == 'd' then
-			color_score = color_score + 13
-		elseif string.lower(value) == 'e' then
-			color_score = color_score + 14
-		elseif string.lower(value) == 'f' then
-			color_score = color_score + 15
-		end
+	-- hex color is invalid
+	if #rgb_table < 3 then
+		return nil
 	end
 
-	return color_score >= color_threshhold and "#000000" or "#ffffff"
+	-- see: https://stackoverflow.com/a/3943023/16807083
+	rgb_table = vim.tbl_map(function(value)
+		value = value / 255
+
+		if value <= 0.04045 then
+			value = value / 12.92
+		else
+			value = ((value + 0.055) / 1.055) ^ 2.4
+		end
+
+		return value
+	end, rgb_table)
+
+	local luminance = (0.2126 * rgb_table[1]) + (0.7152 * rgb_table[2]) + (0.0722 * rgb_table[3])
+
+	return luminance > 0.179 and "#000000" or "#ffffff"
 end
 
 return M
--- lua/nvim-highlight-colors/utils.lua
+++ lua/nvim-highlight-colors/utils.lua
@@ -4,19 +4,16 @@ local buffer_utils = require("nvim-highlight-colors.buffer_utils")
 local M = {}
 
 function M.get_last_row_index()
-	return vim.fn.line('$')
+	return vim.fn.line("$")
 end
 
 function M.get_win_visible_rows(winid)
-	return vim.api.nvim_win_call(
-		winid,
-		function()
-			return {
-				vim.fn.line('w0'),
-				vim.fn.line('w$')
-			}
-		end
-	)
+	return vim.api.nvim_win_call(winid, function()
+		return {
+			vim.fn.line("w0"),
+			vim.fn.line("w$"),
+		}
+	end)
 end
 
 function create_highlight_name(color_value)
@@ -28,23 +25,25 @@ function M.create_window(row, col, bg_color, row_offset)
 	local buf = vim.api.nvim_create_buf(false, true)
 	local window = vim.api.nvim_open_win(buf, false, {
 		relative = "win",
-		bufpos={row, col},
+		bufpos = { row, col },
 		width = 1,
 		height = 1,
 		focusable = false,
 		noautocmd = true,
 		zindex = 1,
-		style= "minimal"
+		style = "minimal",
 	})
-	vim.api.nvim_command("highlight " .. highlight_color_name .. " guibg=" .. colors.get_color_value(bg_color, row_offset))
+	vim.api.nvim_command(
+		"highlight " .. highlight_color_name .. " guibg=" .. colors.get_color_value(bg_color, row_offset)
+	)
 	vim.api.nvim_win_set_option(
 		window,
-		'winhighlight',
-		'Normal:' .. highlight_color_name .. ',FloatBorder:' .. highlight_color_name
+		"winhighlight",
+		"Normal:" .. highlight_color_name .. ",FloatBorder:" .. highlight_color_name
 	)
 
 	local row_content = buffer_utils.get_buffer_contents(row + 1, row + 2)
-	vim.api.nvim_buf_set_lines(buf, 0, 0, true, {string.sub(row_content[1], 0, 1)})
+	vim.api.nvim_buf_set_lines(buf, 0, 0, true, { string.sub(row_content[1], 0, 1) })
 
 	return window
 end
@@ -60,17 +59,10 @@ function M.create_highlight(ns_id, row, start_column, end_column, color, should_
 		local foreground_color = colors.get_foreground_color_from_hex_color(color_value)
 		vim.api.nvim_command(highlight_color_name .. "guibg=" .. color_value .. " guifg=" .. foreground_color)
 	end
-	vim.api.nvim_buf_add_highlight(
-		0,
-		ns_id,
-		highlight_group,
-		row + 1,
-		start_column,
-		end_column
-	)
+	vim.api.nvim_buf_add_highlight(0, ns_id, highlight_group, row + 1, start_column, end_column)
 end
 
-function M.close_windows (windows)
+function M.close_windows(windows)
 	for _, data in pairs(windows) do
 		if vim.api.nvim_win_is_valid(data) then
 			vim.api.nvim_win_close(data, false)
