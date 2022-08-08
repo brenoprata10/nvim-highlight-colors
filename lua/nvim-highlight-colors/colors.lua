db59c89db1c1ae6c75b19695bc03923dbc04112c
NickyTope <nicky.tope@gmail.com>
2022-08-08 16:15:49 +1000

    support for rgb space delimited
    as per:
    https://stylelint.io/user-guide/rules/list/color-function-notation/

--- lua/nvim-highlight-colors/colors.lua
+++ lua/nvim-highlight-colors/colors.lua
@@ -3,108 +3,108 @@ local table_utils  = require("nvim-highlight-colors.table_utils")
 
 local M = {}
 
-M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, ",") ..",?%s*%d*%.?%d*%s*[)]+"
+M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, "[, ]") .. "[, /]?%s*%d*%.?%d*%s*[)]+"
 M.hex_regex = "#[%a%d]+[%a%d]+[%a%d]+"
 M.var_regex = "%-%-[%d%a-_]+"
 M.var_declaration_regex = M.var_regex .. ":%s*" .. M.hex_regex
 M.var_usage_regex = "var%(" .. M.var_regex .. "%)"
 
 function M.get_color_value(color, row_offset)
-	if (M.is_short_hex_color(color)) then
-		return M.convert_short_hex_to_hex(color)
-	end
+  if (M.is_short_hex_color(color)) then
+    return M.convert_short_hex_to_hex(color)
+  end
 
-	if (M.is_rgb_color(color)) then
-		local rgb_table = M.get_rgb_values(color)
-		if (#rgb_table >= 3) then
-			return M.convert_rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
-		end
-	end
+  if (M.is_rgb_color(color)) then
+    local rgb_table = M.get_rgb_values(color)
+    if (#rgb_table >= 3) then
+      return M.convert_rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
+    end
+  end
 
-	if (M.is_var_color(color)) then
-		local var_name = string.match(color, M.var_regex)
-		local var_name_regex = string.gsub(var_name, "%-", "%%-")
-		local var_position = buffer_utils.get_positions_by_regex(
-			{
-				var_name_regex .. ":%s*" .. M.hex_regex,
-				var_name_regex .. ":%s*" .. M.rgb_regex
-			},
-			0,
-			vim.fn.line('$'),
-			row_offset
-		)
-		if (#var_position > 0) then
-			local hex_color = string.match(var_position[1].value, M.hex_regex)
-			local rgb_color = string.match(var_position[1].value, M.rgb_regex)
-			return hex_color and M.get_color_value(hex_color) or M.get_color_value(rgb_color)
-		end
-	end
+  if (M.is_var_color(color)) then
+    local var_name = string.match(color, M.var_regex)
+    local var_name_regex = string.gsub(var_name, "%-", "%%-")
+    local var_position = buffer_utils.get_positions_by_regex(
+      {
+        var_name_regex .. ":%s*" .. M.hex_regex,
+        var_name_regex .. ":%s*" .. M.rgb_regex
+      },
+      0,
+      vim.fn.line('$'),
+      row_offset
+    )
+    if (#var_position > 0) then
+      local hex_color = string.match(var_position[1].value, M.hex_regex)
+      local rgb_color = string.match(var_position[1].value, M.rgb_regex)
+      return hex_color and M.get_color_value(hex_color) or M.get_color_value(rgb_color)
+    end
+  end
 
-	return color
+  return color
 end
 
 function M.convert_rgb_to_hex(r, g, b)
- 	return string.format("#%02X%02X%02X", r, g, b)
+  return string.format("#%02X%02X%02X", r, g, b)
 end
 
 function M.is_short_hex_color(color)
-	return string.len(color) == 4
+  return string.len(color) == 4
 end
 
 function M.is_rgb_color(color)
-	return string.match(color, M.rgb_regex)
+  return string.match(color, M.rgb_regex)
 end
 
 function M.is_var_color(color)
-	return string.match(color, M.var_usage_regex)
+  return string.match(color, M.var_usage_regex)
 end
 
 function M.convert_short_hex_to_hex(color)
-	if (M.is_short_hex_color(color)) then
-		local new_color = "#"
-		for char in color:gmatch"." do
-			if (char ~= '#') then
-				new_color = new_color .. char:rep(2)
-			end
-		end
-		return new_color
-	end
+  if (M.is_short_hex_color(color)) then
+    local new_color = "#"
+    for char in color:gmatch "." do
+      if (char ~= '#') then
+        new_color = new_color .. char:rep(2)
+      end
+    end
+    return new_color
+  end
 
-	return color
+  return color
 end
 
 function M.get_rgb_values(color)
-	local rgb_table = {}
-	for color_number in string.gmatch(color, "%d+") do
-		table.insert(rgb_table, color_number)
-	end
+  local rgb_table = {}
+  for color_number in string.gmatch(color, "%d+") do
+    table.insert(rgb_table, color_number)
+  end
 
-	return rgb_table
+  return rgb_table
 end
 
 function M.get_foreground_color_from_hex_color(color)
-	local color_score = 0
-	local color_threshhold = 40
+  local color_score = 0
+  local color_threshhold = 40
 
-	for value in string.gmatch(color, ".") do
-		if type(value) == "number" then
-			color_score = color_score + value
-		elseif string.lower(value) == 'a' then
-			color_score = color_score + 10
-		elseif string.lower(value) == 'b' then
-			color_score = color_score + 11
-		elseif string.lower(value) == 'c' then
-			color_score = color_score + 12
-		elseif string.lower(value) == 'd' then
-			color_score = color_score + 13
-		elseif string.lower(value) == 'e' then
-			color_score = color_score + 14
-		elseif string.lower(value) == 'f' then
-			color_score = color_score + 15
-		end
-	end
+  for value in string.gmatch(color, ".") do
+    if type(value) == "number" then
+      color_score = color_score + value
+    elseif string.lower(value) == 'a' then
+      color_score = color_score + 10
+    elseif string.lower(value) == 'b' then
+      color_score = color_score + 11
+    elseif string.lower(value) == 'c' then
+      color_score = color_score + 12
+    elseif string.lower(value) == 'd' then
+      color_score = color_score + 13
+    elseif string.lower(value) == 'e' then
+      color_score = color_score + 14
+    elseif string.lower(value) == 'f' then
+      color_score = color_score + 15
+    end
+  end
 
-	return color_score >= color_threshhold and "#000000" or "#ffffff"
+  return color_score >= color_threshhold and "#000000" or "#ffffff"
 end
 
 return M
--- lua/nvim-highlight-colors/utils.lua
+++ lua/nvim-highlight-colors/utils.lua
@@ -4,78 +4,78 @@ local buffer_utils = require("nvim-highlight-colors.buffer_utils")
 local M = {}
 
 function M.get_last_row_index()
-	return vim.fn.line('$')
+  return vim.fn.line('$')
 end
 
 function M.get_win_visible_rows(winid)
-	return vim.api.nvim_win_call(
-		winid,
-		function()
-			return {
-				vim.fn.line('w0'),
-				vim.fn.line('w$')
-			}
-		end
-	)
+  return vim.api.nvim_win_call(
+    winid,
+    function()
+      return {
+        vim.fn.line('w0'),
+        vim.fn.line('w$')
+      }
+    end
+  )
 end
 
 function create_highlight_name(color_value)
-	return string.gsub(color_value, "#", ""):gsub("[(),%s%.-]+", "")
+  return string.gsub(color_value, "#", ""):gsub("[(),%s%.-/]+", "")
 end
 
 function M.create_window(row, col, bg_color, row_offset)
-	local highlight_color_name = create_highlight_name(bg_color)
-	local buf = vim.api.nvim_create_buf(false, true)
-	local window = vim.api.nvim_open_win(buf, false, {
-		relative = "win",
-		bufpos={row, col},
-		width = 1,
-		height = 1,
-		focusable = false,
-		noautocmd = true,
-		zindex = 1,
-		style= "minimal"
-	})
-	vim.api.nvim_command("highlight " .. highlight_color_name .. " guibg=" .. colors.get_color_value(bg_color, row_offset))
-	vim.api.nvim_win_set_option(
-		window,
-		'winhighlight',
-		'Normal:' .. highlight_color_name .. ',FloatBorder:' .. highlight_color_name
-	)
+  local highlight_color_name = create_highlight_name(bg_color)
+  local buf = vim.api.nvim_create_buf(false, true)
+  local window = vim.api.nvim_open_win(buf, false, {
+    relative = "win",
+    bufpos = { row, col },
+    width = 1,
+    height = 1,
+    focusable = false,
+    noautocmd = true,
+    zindex = 1,
+    style = "minimal"
+  })
+  vim.api.nvim_command("highlight " .. highlight_color_name .. " guibg=" .. colors.get_color_value(bg_color, row_offset))
+  vim.api.nvim_win_set_option(
+    window,
+    'winhighlight',
+    'Normal:' .. highlight_color_name .. ',FloatBorder:' .. highlight_color_name
+  )
 
-	local row_content = buffer_utils.get_buffer_contents(row + 1, row + 2)
-	vim.api.nvim_buf_set_lines(buf, 0, 0, true, {string.sub(row_content[1], 0, 1)})
+  local row_content = buffer_utils.get_buffer_contents(row + 1, row + 2)
+  vim.api.nvim_buf_set_lines(buf, 0, 0, true, { string.sub(row_content[1], 0, 1) })
 
-	return window
+  return window
 end
 
 function M.create_highlight(ns_id, row, start_column, end_column, color, should_colorize_foreground)
-	local highlight_group = create_highlight_name(color)
-	local highlight_color_name = "highlight " .. highlight_group .. " "
-	local color_value = colors.get_color_value(color, 2)
+  local highlight_group = create_highlight_name(color)
+  local highlight_color_name = "highlight " .. highlight_group .. " "
+  local color_value = colors.get_color_value(color, 2)
 
-	if should_colorize_foreground then
-		vim.api.nvim_command(highlight_color_name .. "guifg=" .. color_value)
-	else
-		local foreground_color = colors.get_foreground_color_from_hex_color(color_value)
-		vim.api.nvim_command(highlight_color_name .. "guibg=" .. color_value .. " guifg=" .. foreground_color)
-	end
-	vim.api.nvim_buf_add_highlight(
-		0,
-		ns_id,
-		highlight_group,
-		row + 1,
-		start_column,
-		end_column
-	)
+  if should_colorize_foreground then
+    vim.api.nvim_command(highlight_color_name .. "guifg=" .. color_value)
+  else
+    local foreground_color = colors.get_foreground_color_from_hex_color(color_value)
+    vim.api.nvim_command(highlight_color_name .. "guibg=" .. color_value .. " guifg=" .. foreground_color)
+  end
+  vim.api.nvim_buf_add_highlight(
+    0,
+    ns_id,
+    highlight_group,
+    row + 1,
+    start_column,
+    end_column
+  )
 end
 
-function M.close_windows (windows)
-	for _, data in pairs(windows) do
-		if vim.api.nvim_win_is_valid(data) then
-			vim.api.nvim_win_close(data, false)
-		end
-	end
+function M.close_windows(windows)
+  for _, data in pairs(windows) do
+    if vim.api.nvim_win_is_valid(data) then
+      vim.api.nvim_win_close(data, false)
+    end
+  end
 end
 
 return M
