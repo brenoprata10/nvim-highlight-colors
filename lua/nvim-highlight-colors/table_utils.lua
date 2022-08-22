73ffdf499cf8ab0326e0148777bb99fdaffab812
Breno Prata <brenoprata@hotmail.com>
2022-08-22 16:51:37 +0200

    #22 - Fix repeated colors not being displayed in the same row

--- lua/nvim-highlight-colors/table_utils.lua
+++ lua/nvim-highlight-colors/table_utils.lua
@@ -79,4 +79,38 @@ function M.print(node)
     print(output_str)
 end
 
+function M.find(table_param, fun)
+	for index, value in pairs(table_param) do
+		local assertion = fun(value)
+		if assertion then
+			return {index, value}
+		end
+	end
+
+	return nil
+end
+
+function M.find_index(table_param, fun)
+	for index, value in pairs(table_param) do
+		local assertion = fun(value)
+		if assertion then
+			return index
+		end
+	end
+
+	return nil
+end
+
+function M.filter(table_param, fun)
+	local results = {}
+	for index, value in pairs(table_param) do
+		local assertion = fun(value)
+		if assertion then
+			table.insert(results, value)
+		end
+	end
+
+	return results
+end
+
 return M
