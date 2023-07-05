1e528224f1fbcff640c2bca5bff0ecc4b6d9264e
Azad <49314270+Akmadan23@users.noreply.github.com>
2023-07-05 13:51:57 +0200

    Fix regex bug

--- lua/nvim-highlight-colors/color/patterns.lua
+++ lua/nvim-highlight-colors/color/patterns.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, "[,%s]") .. "[,%s/]?%s*%d*%.?%d*%s*[)]+"
-M.hex_regex = "#[%a%d]+[%a%d]+[%a%d]+"
-M.hex_0x_regex = "0x[%a%d]+[%a%d]+[%a%d]+"
+M.hex_regex = "#%x%x%x+"
+M.hex_0x_regex = "0x%x%x%x+"
 M.hsl_regex = "hsla?[(]+" .. string.rep("%s*%d?%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]") .. "[%s,/]?%s*%d*%.?%d*%%?%s*[)]+"
 
 M.var_regex = "%-%-[%d%a-_]+"
