0098822c1107084c22f648bd4227fdb473b698b5
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-09 16:02:01 +0700

    Fix: fixes vim:E976 error when cursor is on a blob

--- lua/nvim-highlight-colors/buffer_utils.lua
+++ lua/nvim-highlight-colors/buffer_utils.lua
@@ -1,4 +1,4 @@
-local table_utils = require "nvim-highlight-colors.table_utils"
+local table_utils = require("nvim-highlight-colors.table_utils")
 
 local M = {}
 
@@ -14,29 +14,29 @@ function M.get_positions_by_regex(patterns, min_row, max_row, row_offset, render
 
 	for _, pattern in pairs(patterns) do
 		for key, value in pairs(content) do
-			for match in string.gmatch(value, pattern) do
-				local row = key + min_row - row_offset
-				local repeated_colors_in_row = table_utils.filter(
-					positions,
-					function(position)
+			-- Fixes vim:E976 error when cursor is on a blob
+			if vim.fn.type(value) ~= vim.v.t_blob then
+				for match in string.gmatch(value, pattern) do
+					local row = key + min_row - row_offset
+					local repeated_colors_in_row = table_utils.filter(positions, function(position)
 						return position.value == match and position.row == row
+					end)
+					local last_repeated_color = repeated_colors_in_row[#repeated_colors_in_row]
+					local column_offset = last_repeated_color and last_repeated_color.end_column or nil
+
+					local pattern_without_usage_regex = M.remove_color_usage_pattern(match)
+					local start_column = vim.fn.match(value, pattern_without_usage_regex, column_offset)
+					local end_column = vim.fn.matchend(value, pattern_without_usage_regex, column_offset)
+					local starting_row = render == "first_column" and 0 or -1
+
+					if row >= starting_row then
+						table.insert(positions, {
+							value = match,
+							row = row,
+							start_column = start_column,
+							end_column = end_column,
+						})
 					end
-				)
-				local last_repeated_color = repeated_colors_in_row[#repeated_colors_in_row]
-				local column_offset = last_repeated_color and last_repeated_color.end_column or nil
-
-				local pattern_without_usage_regex = M.remove_color_usage_pattern(match)
-				local start_column = vim.fn.match(value, pattern_without_usage_regex, column_offset)
-				local end_column = vim.fn.matchend(value, pattern_without_usage_regex, column_offset)
-				local starting_row = render == 'first_column' and 0 or -1
-
-				if (row >= starting_row) then
-					table.insert(positions, {
-						value = match,
-						row = row,
-						start_column = start_column,
-						end_column = end_column
-					})
 				end
 			end
 		end
@@ -47,9 +47,7 @@ end
 
 function M.remove_color_usage_pattern(match)
 	local _, end_index = string.find(match, M.color_usage_regex)
-	return end_index
-		and string.sub(match, end_index + 1, string.len(match))
-		or match
+	return end_index and string.sub(match, end_index + 1, string.len(match)) or match
 end
 
 return M
