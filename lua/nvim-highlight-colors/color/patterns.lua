b1443418b1f60dd26ca65007fedd596233805ec2
Michael Vo <zax@zaxu.xyz>
2023-12-30 17:25:29 +1100

    fix: only highlight hex colours at word boundaries

--- lua/nvim-highlight-colors/color/patterns.lua
+++ lua/nvim-highlight-colors/color/patterns.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, "[,%s]") .. "[,%s/]?%s*%d*%.?%d*%s*[)]+"
-M.hex_regex = "#%x%x%x+"
-M.hex_0x_regex = "0x%x%x%x+"
+M.hex_regex = "#%x%x%x+%f[^%w]"
+M.hex_0x_regex = "%f[%w]0x%x%x%x+%f[^%w]"
 M.hsl_regex = "hsla?[(]+" .. string.rep("%s*%d?%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]") .. "[%s,/]?%s*%d*%.?%d*%%?%s*[)]+"
 
 M.var_regex = "%-%-[%d%a-_]+"
