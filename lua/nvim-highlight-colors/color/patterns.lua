96eeb1e593a7d7cd54080d8e16478df954af1f6b
OxKahi <waifumaximalist@gmail.com>
2025-05-14 23:56:24 +0800

    fix formatting

--- lua/nvim-highlight-colors/color/patterns.lua
+++ lua/nvim-highlight-colors/color/patterns.lua
@@ -1,33 +1,30 @@
 local M = {}
 
-M.rgb_regex = 'rgba?[(]+' .. string.rep('%s*%d+%s*', 3, '[,%s]') .. '[,%s/]?%s*%d*%.?%d*%%?%s*[)]+'
-M.hex_regex = '#%x%x%x+%f[^%w_]'
-M.hex_0x_regex = '%f[%w_]0x%x%x%x+%f[^%w_]'
-M.hsl_regex = 'hsla?[(]+'
-  .. string.rep('%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*', 3, '[,%s]')
-  .. '[%s,/]?%s*%d*%.?%d*%%?%s*[)]+'
+M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, "[,%s]") .. "[,%s/]?%s*%d*%.?%d*%%?%s*[)]+"
+M.hex_regex = "#%x%x%x+%f[^%w_]"
+M.hex_0x_regex = "%f[%w_]0x%x%x%x+%f[^%w_]"
+M.hsl_regex = "hsla?[(]+" .. string.rep("%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]") .. "[%s,/]?%s*%d*%.?%d*%%?%s*[)]+"
 -- Matches: `: 0 69% 69%`
-M.hsl_without_func_regex = ':' .. string.rep('%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*', 3, '[,%s]')
-
+M.hsl_without_func_regex = ":" .. string.rep("%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]")
 M.oklch_regex = 'oklch%(+%s*%d*%.?%d+%%?%s*%s+%d*%.?%d+%%?%s*%s+%d*%.?%d+d?e?g?t?u?r?n?%s*[%s,/]?%s*%d*%.?%d*%%?%s*%)+'
 
-M.var_regex = '%-%-[%d%a-_]+'
-M.var_declaration_regex = M.var_regex .. ':%s*' .. M.hex_regex
-M.var_usage_regex = 'var%(' .. M.var_regex .. '%)'
+M.var_regex = "%-%-[%d%a-_]+"
+M.var_declaration_regex = M.var_regex .. ":%s*" .. M.hex_regex
+M.var_usage_regex = "var%(" .. M.var_regex .. "%)"
 
-M.tailwind_prefix = '!?%a+'
+M.tailwind_prefix = "!?%a+"
 
-M.ansi_regex = '\\033%[%d;%d%dm'
+M.ansi_regex = "\\033%[%d;%d%dm"
 
 ---Checks whether a color is short hex
 ---@param color string
 ---@usage is_short_hex_color("#FFF") => Returns true
 ---@return boolean
 function M.is_short_hex_color(color)
-  if string.match(color, M.hex_regex) then
-    return string.len(color) == 4
-  end
-  return false
+	if string.match(color, M.hex_regex) then
+		return string.len(color) == 4
+	end
+	return false
 end
 
 ---Checks whether a color is hex
@@ -35,10 +32,10 @@ end
 ---@usage is_hex_color("#FFFFFF") => Returns true
 ---@return boolean
 function M.is_hex_color(color)
-  if string.match(color, M.hex_regex) then
-    return string.len(color) == 7
-  end
-  return false
+	if string.match(color, M.hex_regex) then
+		return string.len(color) == 7
+	end
+	return false
 end
 
 ---Checks whether a color is hex with alpha data
@@ -46,7 +43,7 @@ end
 ---@usage is_alpha_layer_hex("#FFFFFFFF") => Returns true
 ---@return boolean
 function M.is_alpha_layer_hex(color)
-  return string.match(color, M.hex_regex) ~= nil and string.len(color) == 9
+	return string.match(color, M.hex_regex) ~= nil and string.len(color) == 9
 end
 
 ---Checks whether a color is rgb
@@ -54,7 +51,7 @@ end
 ---@usage is_rgb_color("rgb(255, 255, 255)") => Returns true
 ---@return boolean
 function M.is_rgb_color(color)
-  return string.match(color, M.rgb_regex) ~= nil
+	return string.match(color, M.rgb_regex) ~= nil
 end
 
 ---Checks whether a color is hsl
@@ -62,7 +59,7 @@ end
 ---@usage is_hsl_color("hsl(240, 100%, 50%)") => Returns true
 ---@return boolean
 function M.is_hsl_color(color)
-  return string.match(color, M.hsl_regex) ~= nil
+	return string.match(color, M.hsl_regex) ~= nil
 end
 
 -- Checks wether a color is a hsl without function color
@@ -70,7 +67,7 @@ end
 ---@usage is_hsl_without_func_color(": 0 0% 100%;") => Returns true
 ---@return boolean
 function M.is_hsl_without_func_color(color)
-  return string.match(color, M.hsl_without_func_regex) ~= nil
+	return string.match(color, M.hsl_without_func_regex) ~= nil
 end
 
 ---Checks whether a color is a CSS var color
@@ -78,7 +75,7 @@ end
 ---@usage is_var_color("var(--css-color)") => Returns true
 ---@return boolean
 function M.is_var_color(color)
-  return string.match(color, M.var_usage_regex) ~= nil
+	return string.match(color, M.var_usage_regex) ~= nil
 end
 
 ---Checks whether a color is a custom color
@@ -86,13 +83,13 @@ end
 ---@usage is_custom_color("custom-color", {{label = 'custom%-color', color = '#FFFFFF'}}) => Returns true
 ---@return boolean
 function M.is_custom_color(color, custom_colors)
-  for _, custom_color in pairs(custom_colors) do
-    if color == custom_color.label:gsub('%%', '') then
-      return true
-    end
-  end
+	for _, custom_color in pairs(custom_colors) do
+		if color == custom_color.label:gsub("%%", "") then
+			return true
+		end
+	end
 
-  return false
+	return false
 end
 
 ---Checks whether a color is a named color e.g. 'blue', 'green'
@@ -100,13 +97,13 @@ end
 ---@usage is_named_color({M.get_tailwind_named_color_pattern()}, "--bg-white") => Returns true
 ---@return boolean
 function M.is_named_color(named_color_patterns, color)
-  for _, pattern in pairs(named_color_patterns) do
-    if string.match(color, pattern) then
-      return true
-    end
-  end
+	for _, pattern in pairs(named_color_patterns) do
+		if string.match(color, pattern) then
+			return true
+		end
+	end
 
-  return false
+	return false
 end
 
 ---Checks whether a color is a ansi color
@@ -114,7 +111,7 @@ end
 ---@usage is_ansi_color("\\033[1;37m") => Returns true
 ---@return boolean
 function M.is_ansi_color(color)
-  return string.match(color, M.ansi_regex) ~= nil
+	return string.match(color, M.ansi_regex) ~= nil
 end
 
 ---Checks whether a color is oklch
--- lua/nvim-highlight-colors/color/utils.lua
+++ lua/nvim-highlight-colors/color/utils.lua
@@ -1,9 +1,9 @@
-local buffer_utils = require('nvim-highlight-colors.buffer_utils')
-local css_named_colors = require('nvim-highlight-colors.named-colors.css')
-local tailwind_named_colors = require('nvim-highlight-colors.named-colors.tailwind')
-local converters = require('nvim-highlight-colors.color.converters')
-local patterns = require('nvim-highlight-colors.color.patterns')
-local ansi_named_colors = require('nvim-highlight-colors.named-colors.ansi')
+local buffer_utils = require("nvim-highlight-colors.buffer_utils")
+local css_named_colors = require("nvim-highlight-colors.named-colors.css")
+local tailwind_named_colors = require("nvim-highlight-colors.named-colors.tailwind")
+local converters = require("nvim-highlight-colors.color.converters")
+local patterns = require("nvim-highlight-colors.color.patterns")
+local ansi_named_colors = require("nvim-highlight-colors.named-colors.ansi")
 
 local M = {}
 
@@ -14,71 +14,64 @@ local M = {}
 ---@param enable_short_hex? boolean
 ---@usage get_color_value("rgb(255, 255, 255)") => Returns "#FFFFFF"
 ---@return string | nil
-function M.get_color_value(color, row_offset, custom_colors, enable_short_hex)
-  if enable_short_hex and patterns.is_short_hex_color(color) then
-    return converters.short_hex_to_hex(color)
-  end
+function M.get_color_value(color, row_offset, custom_colors, enable_short_hex )
+	if (enable_short_hex and patterns.is_short_hex_color(color)) then
+		return converters.short_hex_to_hex(color)
+	end
 
-  if patterns.is_alpha_layer_hex(color) then
-    return string.sub(color, 1, 7)
-  end
+	if (patterns.is_alpha_layer_hex(color)) then
+		return string.sub(color, 1, 7)
+	end
 
-  if patterns.is_rgb_color(color) then
-    local rgb_table = M.get_rgb_values(color)
-    if #rgb_table >= 3 then
-      return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
-    end
-  end
+	if (patterns.is_rgb_color(color)) then
+		local rgb_table = M.get_rgb_values(color)
+		if (#rgb_table >= 3) then
+			return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
+		end
+	end
 
-  if patterns.is_hsl_color(color) then
-    local hsl_table = M.get_hsl_values(color)
-    local rgb_table = converters.hsl_to_rgb(hsl_table[1], hsl_table[2], hsl_table[3])
-    return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
-  end
+	if (patterns.is_hsl_color(color)) then
+		local hsl_table = M.get_hsl_values(color)
+		local rgb_table = converters.hsl_to_rgb(hsl_table[1], hsl_table[2], hsl_table[3])
+		return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
+	end
 
-  if patterns.is_hsl_without_func_color(color) then
-    local hsl_table = M.get_hsl_without_func_values(color)
-    local rgb_table = converters.hsl_to_rgb(hsl_table[1], hsl_table[2], hsl_table[3])
-    return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
-  end
+	if (patterns.is_hsl_without_func_color(color)) then
+		local hsl_table = M.get_hsl_without_func_values(color)
+		local rgb_table = converters.hsl_to_rgb(hsl_table[1], hsl_table[2], hsl_table[3])
+		return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
+	end
 
-  if patterns.is_named_color({ M.get_css_named_color_pattern() }, color) then
-    return M.get_css_named_color_value(color)
-  end
+	if (patterns.is_named_color({M.get_css_named_color_pattern()}, color)) then
+		return M.get_css_named_color_value(color)
+	end
 
-  if patterns.is_ansi_color(color) then
-    return M.get_ansi_named_color_value(color)
-  end
+	if patterns.is_ansi_color(color) then
+		return M.get_ansi_named_color_value(color)
+	end
 
-  if patterns.is_named_color({ M.get_tailwind_named_color_pattern() }, color) then
-    local tailwind_color = M.get_tailwind_named_color_value(color)
-    if tailwind_color ~= nil then
-      return tailwind_color
-    end
-  end
+	if (patterns.is_named_color({M.get_tailwind_named_color_pattern()}, color)) then
+		local tailwind_color = M.get_tailwind_named_color_value(color)
+		if (tailwind_color ~= nil) then
+			return tailwind_color
+		end
+	end
 
-  if row_offset ~= nil and patterns.is_var_color(color) then
-    return M.get_css_var_color(color, row_offset)
-  end
+	if (row_offset ~= nil and patterns.is_var_color(color)) then
+		return M.get_css_var_color(color, row_offset)
+	end
 
-  if custom_colors ~= nil and patterns.is_custom_color(color, custom_colors) then
-    return M.get_custom_color(color, custom_colors)
-  end
+	if (custom_colors ~= nil and patterns.is_custom_color(color, custom_colors)) then
+		return M.get_custom_color(color, custom_colors)
+	end
 
-  -- Add this conditional for OKLCH after the other color format checks
-  if patterns.is_oklch_color(color) then
-    local oklch_table = M.get_oklch_values(color)
-    local rgb_table = converters.oklch_to_rgb(oklch_table[1], oklch_table[2], oklch_table[3])
-    return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
-  end
+	local hex_color = color:gsub("0x", "#")
 
-  local hex_color = color:gsub('0x', '#')
+	if (patterns.is_hex_color(hex_color)) then
+		return hex_color
+	end
 
-  if patterns.is_hex_color(hex_color) then
-    return hex_color
-  end
-
-  return nil
+	return nil
 end
 
 ---Returns the rgb table of a rgb string
@@ -86,12 +79,12 @@ end
 ---@usage get_rgb_values("rgb(92, 92, 255)") => Returns {'92', '92', '255'}
 ---@return string[]
 function M.get_rgb_values(color)
-  local rgb_table = {}
-  for color_number in string.gmatch(color, '%d+') do
-    table.insert(rgb_table, color_number)
-  end
+	local rgb_table = {}
+	for color_number in string.gmatch(color, "%d+") do
+		table.insert(rgb_table, color_number)
+	end
 
-  return rgb_table
+	return rgb_table
 end
 
 ---Returns the hsl table of a hsl string
@@ -99,12 +92,12 @@ end
 ---@usage get_hsl_values("hsl(240, 100%, 68%)") => Returns {'240', '100', '68'}
 ---@return string[]
 function M.get_hsl_values(color)
-  local hsl_table = {}
-  for color_number in string.gmatch(color, '%d*%.?%d+') do
-    table.insert(hsl_table, color_number)
-  end
+	local hsl_table = {}
+	for color_number in string.gmatch(color, "%d*%.?%d+") do
+		table.insert(hsl_table, color_number)
+	end
 
-  return hsl_table
+	return hsl_table
 end
 
 ---Returns the hsl table from a custom property HSL format (e.g. "--name: 0 84.2% 60.2%;")
@@ -112,15 +105,15 @@ end
 ---@usage get_hsl_without_func_values("--name: 0 0% 100%;") => Returns {'0', '0', '100'}
 ---@return string[]
 function M.get_hsl_without_func_values(color)
-  local hsl_table = {}
-  -- Remove the colon and any leading whitespace before matching numbers
-  local clean_color = color:match(':%s*(.+)')
-  if clean_color then
-    for value in clean_color:gmatch('%d*%.?%d+') do
-      table.insert(hsl_table, value)
+    local hsl_table = {}
+    -- Remove the colon and any leading whitespace before matching numbers
+    local clean_color = color:match(":%s*(.+)")
+    if clean_color then
+        for value in clean_color:gmatch("%d*%.?%d+") do
+            table.insert(hsl_table, value)
+        end
     end
-  end
-  return hsl_table
+    return hsl_table
 end
 
 ---Returns the hex value of a CSS color
@@ -128,8 +121,8 @@ end
 ---@usage get_css_named_color_value('red') => Returns '#FF0000'
 ---@return string
 function M.get_css_named_color_value(color)
-  local color_name = string.match(color, '%a+')
-  return css_named_colors[color_name]
+	local color_name = string.match(color, "%a+")
+	return css_named_colors[color_name]
 end
 
 ---Returns the hex value of a tailwind color
@@ -137,20 +130,20 @@ end
 ---@usage get_tailwind_named_color_value('bg-white') => Returns '#FFFFFF'
 ---@return string|nil
 function M.get_tailwind_named_color_value(color)
-  local tailwind_color_name = color
-  -- Removing tailwind prefix from color name: text-slate-500 -> slate-500
-  local _, end_index = string.find(tailwind_color_name, patterns.tailwind_prefix .. '%-')
-  if end_index then
-    tailwind_color_name = string.sub(tailwind_color_name, end_index + 1, string.len(tailwind_color_name))
-  end
-  local tailwind_color = tailwind_named_colors[tailwind_color_name]
-  if tailwind_color == nil then
-    return nil
-  end
-  local rgb_table = M.get_rgb_values(tailwind_color)
-  if #rgb_table >= 3 then
-    return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
-  end
+	local tailwind_color_name = color
+	-- Removing tailwind prefix from color name: text-slate-500 -> slate-500
+	local _, end_index = string.find(tailwind_color_name, patterns.tailwind_prefix .. "%-")
+	if end_index then
+		tailwind_color_name = string.sub(tailwind_color_name, end_index + 1, string.len(tailwind_color_name))
+	end
+	local tailwind_color = tailwind_named_colors[tailwind_color_name]
+	if tailwind_color == nil then
+		return nil
+	end
+	local rgb_table = M.get_rgb_values(tailwind_color)
+	if (#rgb_table >= 3) then
+		return converters.rgb_to_hex(rgb_table[1], rgb_table[2], rgb_table[3])
+	end
 end
 
 ---Returns the hex value of a python ansi color
@@ -158,26 +151,26 @@ end
 ---@usage get_ansi_named_color_value("\\033[1;37m") => Returns '#FFFFFF'
 ---@return string|nil
 function M.get_ansi_named_color_value(color)
-  local color_code = nil
-  if string.match(color, patterns.ansi_regex) then
-    color_code = string.match(color, '([0-9;]+)m')
-  end
-  if ansi_named_colors[color_code] then
-    return tostring(ansi_named_colors[color_code])
-  end
-  return nil
+	local color_code = nil
+	if string.match(color, patterns.ansi_regex) then
+		color_code = string.match(color, "([0-9;]+)m")
+	end
+	if ansi_named_colors[color_code] then
+		return tostring(ansi_named_colors[color_code])
+	end
+	return nil
 end
 
 ---Returns a pattern for tailwind colors
 ---@return string
 function M.get_tailwind_named_color_pattern()
-  return patterns.tailwind_prefix .. '%-%a+[%-%d+]*'
+	return patterns.tailwind_prefix .. "%-%a+[%-%d+]*"
 end
 
 ---Returns a pattern for CSS colors
 ---@return string
 function M.get_css_named_color_pattern()
-  return buffer_utils.color_usage_regex .. '%a+'
+	return buffer_utils.color_usage_regex .. "%a+"
 end
 
 ---Returns the hex value of a custom color
@@ -186,86 +179,91 @@ end
 ---@usage get_custom_color('custom-white', {{label = 'custom%-white', color = '#FFFFFF'}}) => Returns '#FFFFFF'
 ---@return string|nil
 function M.get_custom_color(color, custom_colors)
-  for _, custom_color in pairs(custom_colors) do
-    if color == custom_color.label:gsub('%%', '') then
-      return M.get_color_value(custom_color.color)
-    end
-  end
+	for _, custom_color in pairs(custom_colors) do
+		if color == custom_color.label:gsub("%%", "") then
+			return M.get_color_value(custom_color.color)
+		end
+	end
 
-  return nil
+	return nil
 end
 
 ---Returns the hex value of a CSS variable
 ---@param color string --my-css-variable-name
 ---@param row_offset number
----@usage get_css_var_color("--css-variable-name", 0) => Returns "#000000"
+---@usage get_css_var_color("--css-variable-name", 0) => Returns "#000000" 
 ---@return string|nil
 function M.get_css_var_color(color, row_offset)
-  local var_name = string.match(color, patterns.var_regex)
-  local var_name_regex = string.gsub(var_name, '%-', '%%-')
-  local value_patterns = {
-    patterns.hex_regex,
-    patterns.rgb_regex,
-    patterns.hsl_regex,
-    patterns.hsl_without_func_regex:gsub('^:%s*', ''),
-  }
-  local var_patterns = {}
+	local var_name = string.match(color, patterns.var_regex)
+	local var_name_regex = string.gsub(var_name, "%-", "%%-")
+	local value_patterns = {
+		patterns.hex_regex,
+		patterns.rgb_regex,
+		patterns.hsl_regex,
+		patterns.hsl_without_func_regex:gsub("^:%s*", "")
+	}
+	local var_patterns = {}
 
-  for _, pattern in pairs(value_patterns) do
-    table.insert(var_patterns, var_name_regex .. ':%s*' .. pattern)
-  end
-  for _, css_color_pattern in pairs({ M.get_css_named_color_pattern() }) do
-    table.insert(var_patterns, var_name_regex .. css_color_pattern)
-  end
+	for _, pattern in pairs(value_patterns) do
+		table.insert(var_patterns, var_name_regex .. ":%s*" .. pattern)
+	end
+	for _, css_color_pattern in pairs({M.get_css_named_color_pattern()}) do
+		table.insert(var_patterns, var_name_regex .. css_color_pattern)
+	end
 
-  local var_position = buffer_utils.get_positions_by_regex(var_patterns, 0, vim.fn.line('$'), 0, row_offset)
+	local var_position = buffer_utils.get_positions_by_regex(var_patterns, 0, vim.fn.line('$'), 0, row_offset)
 
-  if #var_position > 0 then
-    local hex_color = string.match(var_position[1].value, patterns.hex_regex)
-    local rgb_color = string.match(var_position[1].value, patterns.rgb_regex)
-    local hsl_color = string.match(var_position[1].value, patterns.hsl_regex)
-    local hsl_without_func_color = string.match(var_position[1].value, patterns.hsl_without_func_regex)
-    if hex_color then
-      return M.get_color_value(hex_color)
-    elseif rgb_color then
-      return M.get_color_value(rgb_color)
-    elseif hsl_color then
-      return M.get_color_value(hsl_color)
-    elseif hsl_without_func_color then
-      return M.get_color_value(hsl_without_func_color)
-    else
-      return M.get_css_named_color_value(string.gsub(var_position[1].value, var_name_regex, ''))
-    end
-  end
+	if (#var_position > 0) then
+		local hex_color = string.match(var_position[1].value, patterns.hex_regex)
+		local rgb_color = string.match(var_position[1].value, patterns.rgb_regex)
+		local hsl_color = string.match(var_position[1].value, patterns.hsl_regex)
+		local hsl_without_func_color = string.match(var_position[1].value, patterns.hsl_without_func_regex)
+		if hex_color then
+			return M.get_color_value(hex_color)
+		elseif rgb_color then
+			return M.get_color_value(rgb_color)
+		elseif hsl_color then
+			return M.get_color_value(hsl_color)
+		elseif hsl_without_func_color then
+			return M.get_color_value(hsl_without_func_color)
+		else
+			return M.get_css_named_color_value(
+			string.gsub(var_position[1].value, var_name_regex, "")
+			)
+		end
+	end
 
-  return nil
+	return nil
 end
 
 ---Returns a contrast friendly color that matches the current color for reading purposes
 ---@param color string
----@usage get_foreground_color_from_hex_color("#FFFFFF") => Returns "#000000"
+---@usage get_foreground_color_from_hex_color("#FFFFFF") => Returns "#000000" 
 ---@return string|nil
 function M.get_foreground_color_from_hex_color(color)
-  local rgb_table = converters.hex_to_rgb(color)
+	local rgb_table = converters.hex_to_rgb(color)
 
-  if rgb_table == nil or #rgb_table < 3 then
-    return nil
-  end
+	if rgb_table == nil or #rgb_table < 3 then
+		return nil
+	end
 
-  -- see: https://stackoverflow.com/a/3943023/16807083
-  rgb_table = vim.tbl_map(function(value)
-    value = value / 255
+	-- see: https://stackoverflow.com/a/3943023/16807083
+	rgb_table = vim.tbl_map(
+		function(value)
+			value = value / 255
 
-    if value <= 0.04045 then
-      return value / 12.92
-    end
+			if value <= 0.04045 then
+				return value / 12.92
+			end
 
-    return ((value + 0.055) / 1.055) ^ 2.4
-  end, rgb_table)
+			return ((value + 0.055) / 1.055) ^ 2.4
+		end,
+		rgb_table
+	)
 
-  local luminance = (0.2126 * rgb_table[1]) + (0.7152 * rgb_table[2]) + (0.0722 * rgb_table[3])
+	local luminance = (0.2126 * rgb_table[1]) + (0.7152 * rgb_table[2]) + (0.0722 * rgb_table[3])
 
-  return luminance > 0.179 and '#000000' or '#ffffff'
+	return luminance > 0.179 and "#000000" or "#ffffff"
 end
 
 ---Returns the oklch table of an oklch string
@@ -278,7 +276,6 @@ function M.get_oklch_values(color)
     table.insert(oklch_table, color_number)
   end
 
-  -- Ensure we have at least 3 values (L, C, H)
   while #oklch_table < 3 do
     table.insert(oklch_table, '0')
   end
