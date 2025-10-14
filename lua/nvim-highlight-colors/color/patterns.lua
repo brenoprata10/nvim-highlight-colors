b7a6b164349f3db25e8f62ddbe2c5a133b6062e0
mohammed <mohammedredha908@gmail.com>
2025-10-14 16:57:17 +0300

    add oklch support

--- lua/nvim-highlight-colors/color/patterns.lua
+++ lua/nvim-highlight-colors/color/patterns.lua
@@ -3,7 +3,9 @@ local M = {}
 M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, "[,%s]") .. "[,%s/]?%s*%d*%.?%d*%%?%s*[)]+"
 M.hex_regex = "#%x%x%x+%f[^%w_-]"
 M.hex_0x_regex = "%f[%w_]0x%x%x%x+%f[^%w_]"
-M.hsl_regex = "hsla?[(]+" .. string.rep("%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]") .. "[%s,/]?%s*%d*%.?%d*%%?%s*[)]+"
+M.hsl_regex = "hsla?[(]+" ..
+    string.rep("%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]") .. "[%s,/]?%s*%d*%.?%d*%%?%s*[)]+"
+M.oklch_regex = "oklch%(%s*[%d%.]+%%?%s+[%d%.]+%s+[%d%.]+%s*[/%s]*[%d%.]*%%?%s*%)"
 -- Matches: `: 0 69% 69%`
 M.hsl_without_func_regex = ":" .. string.rep("%s*%d*%.?%d+%%?d?e?g?t?u?r?n?%s*", 3, "[,%s]")
 
@@ -23,10 +25,10 @@ M.xtermTrueColor_regex = "\\033%[[0-9;]*[34]8;2;%d?%d?%d;%d?%d?%d;%d?%d?%dm"
 ---@usage is_short_hex_color("#FFF") => Returns true
 ---@return boolean
 function M.is_short_hex_color(color)
-	if string.match(color, M.hex_regex) then
-		return string.len(color) == 4
-	end
-	return false
+    if string.match(color, M.hex_regex) then
+        return string.len(color) == 4
+    end
+    return false
 end
 
 ---Checks whether a color is short hex with alpha data
@@ -34,7 +36,7 @@ end
 ---@usage is_alpha_layer_short_hex("#FFFF") => Returns true
 ---@return boolean
 function M.is_alpha_layer_short_hex(color)
-	return string.match(color, M.hex_regex) ~= nil and string.len(color) == 5
+    return string.match(color, M.hex_regex) ~= nil and string.len(color) == 5
 end
 
 ---Checks whether a color is hex
@@ -42,10 +44,10 @@ end
 ---@usage is_hex_color("#FFFFFF") => Returns true
 ---@return boolean
 function M.is_hex_color(color)
-	if string.match(color, M.hex_regex) then
-		return string.len(color) == 7
-	end
-	return false
+    if string.match(color, M.hex_regex) then
+        return string.len(color) == 7
+    end
+    return false
 end
 
 ---Checks whether a color is hex with alpha data
@@ -53,7 +55,7 @@ end
 ---@usage is_alpha_layer_hex("#FFFFFFFF") => Returns true
 ---@return boolean
 function M.is_alpha_layer_hex(color)
-	return string.match(color, M.hex_regex) ~= nil and string.len(color) == 9
+    return string.match(color, M.hex_regex) ~= nil and string.len(color) == 9
 end
 
 ---Checks whether a color is rgb
@@ -61,7 +63,15 @@ end
 ---@usage is_rgb_color("rgb(255, 255, 255)") => Returns true
 ---@return boolean
 function M.is_rgb_color(color)
-	return string.match(color, M.rgb_regex) ~= nil
+    return string.match(color, M.rgb_regex) ~= nil
+end
+
+---Checks whether a color is oklch
+---@param color string
+---@usage is_oklch_color("oklch(1 0 0)")
+---@return boolean
+function M.is_oklch_color(color)
+    return string.match(color, M.oklch_regex) ~= nil
 end
 
 ---Checks whether a color is hsl
@@ -69,7 +79,7 @@ end
 ---@usage is_hsl_color("hsl(240, 100%, 50%)") => Returns true
 ---@return boolean
 function M.is_hsl_color(color)
-	return string.match(color, M.hsl_regex) ~= nil
+    return string.match(color, M.hsl_regex) ~= nil
 end
 
 -- Checks wether a color is a hsl without function color
@@ -77,7 +87,7 @@ end
 ---@usage is_hsl_without_func_color(": 0 0% 100%;") => Returns true
 ---@return boolean
 function M.is_hsl_without_func_color(color)
-	return string.match(color, M.hsl_without_func_regex) ~= nil
+    return string.match(color, M.hsl_without_func_regex) ~= nil
 end
 
 ---Checks whether a color is a CSS var color
@@ -85,7 +95,7 @@ end
 ---@usage is_var_color("var(--css-color)") => Returns true
 ---@return boolean
 function M.is_var_color(color)
-	return string.match(color, M.var_usage_regex) ~= nil
+    return string.match(color, M.var_usage_regex) ~= nil
 end
 
 ---Checks whether a color is a custom color
@@ -93,13 +103,13 @@ end
 ---@usage is_custom_color("custom-color", {{label = 'custom%-color', color = '#FFFFFF'}}) => Returns true
 ---@return boolean
 function M.is_custom_color(color, custom_colors)
-	for _, custom_color in pairs(custom_colors) do
-		if color == custom_color.label:gsub("%%", "") then
-			return true
-		end
-	end
+    for _, custom_color in pairs(custom_colors) do
+        if color == custom_color.label:gsub("%%", "") then
+            return true
+        end
+    end
 
-	return false
+    return false
 end
 
 ---Checks whether a color is a named color e.g. 'blue', 'green'
@@ -107,13 +117,13 @@ end
 ---@usage is_named_color({M.get_tailwind_named_color_pattern()}, "--bg-white") => Returns true
 ---@return boolean
 function M.is_named_color(named_color_patterns, color)
-	for _, pattern in pairs(named_color_patterns) do
-		if string.match(color, pattern) then
-			return true
-		end
-	end
+    for _, pattern in pairs(named_color_patterns) do
+        if string.match(color, pattern) then
+            return true
+        end
+    end
 
-	return false
+    return false
 end
 
 ---Checks whether a color is a ansi color
@@ -121,7 +131,7 @@ end
 ---@usage is_ansi_color("\\033[1;37m") => Returns true
 ---@return boolean
 function M.is_ansi_color(color)
-	return string.match(color, M.ansi_regex) ~= nil
+    return string.match(color, M.ansi_regex) ~= nil
 end
 
 ---Checks whether a color is one of xterm's 256 colors (8bit). Accepts modifiers
